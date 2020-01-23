360ceaf71545e6d293732037b1d76929d3c16aa6
jojo0severo <joaovbsevero@gmail.com>
2020-01-23 18:52:27 -0300

    Final version of SQL structure

--- resources/schema.sql
+++ resources/schema.sql
@@ -1,159 +1,113 @@
+CREATE TABLE Address (
+  id                 INTEGER       PRIMARY KEY,
+  street_name        VARCHAR (150) NOT NULL,
+  neighbourhood_name VARCHAR (150) NOT NULL,
+  state_name         VARCHAR (60)  NOT NULL,
+  country_name       VARCHAR (40)  NOT NULL,
+  UNIQUE (street_name, neighbourhood_name, state_name, country_name)
+);
 
-
-CREATE TABLE Complex (
-    id   INTEGER      PRIMARY KEY AUTOINCREMENT,
-    name VARCHAR (50) NOT NULL,
-    UNIQUE (name)
+CREATE TABLE Condominium (
+  id               INTEGER      PRIMARY KEY AUTO_INCREMENT,
+  condominium_name VARCHAR (60) NOT NULL,
+  street_number    INTEGER      NOT NULL,
+  address_id       INTEGER      REFERENCES Address(id),
+  UNIQUE(condominium_name, address_id)
 );
 
 CREATE TABLE Tower (
-    id         INTEGER     PRIMARY KEY AUTOINCREMENT,
-    name       VARCHAR (2) NOT NULL,
-    complex_id INTEGER     NOT NULL,
-    UNIQUE (name, complex_id),
-    FOREIGN KEY (complex_id) REFERENCES Complex(id) ON DELETE CASCADE
+  id             INTEGER      PRIMARY KEY,
+  tower_name     VARCHAR (10) NOT NULL,
+  condominium_id INTEGER      REFERENCES Condominium(id),
+  UNIQUE (tower_name, condominium_id)
 );
 
 CREATE TABLE Apartment (
-    id       INTEGER     PRIMARY KEY AUTOINCREMENT,
-    number   INTEGER     NOT NULL,
-    tower_id INTEGER     NOT NULL,
-    UNIQUE (number, tower_id),
-    FOREIGN KEY (tower_id) REFERENCES Tower(id) ON DELETE CASCADE
+  id         INTEGER PRIMARY KEY,
+  apt_number INTEGER NOT NULL,
+  tower_id   INTEGER REFERENCES Tower(id),
+  UNIQUE(apt_number, tower_id)
 );
 
 CREATE TABLE Resident (
-    id             INTEGER      PRIMARY KEY AUTOINCREMENT,
-    cpf            VARCHAR (11) NOT NULL,
-    name           VARCHAR (50) NOT NULL,
-    email          VARCHAR (50) NOT NULL,
-    contact_number VARCHAR (13) NOT NULL,
-    apt_id         INTEGER      NOT NULL,
-    UNIQUE (cpf, apt_id),
-    FOREIGN KEY (apt_id) REFERENCES Apartment(id) ON DELETE CASCADE
+  id            INTEGER      PRIMARY KEY,
+  cpf           VARCHAR (11) NOT NULL,
+  name          VARCHAR (50) NOT NULL,
+  birthday      DATE         NOT NULL,
+  photo_address VARCHAR (200),
+  apartment_id  INTEGER      REFERENCES Apartment(id),
+  UNIQUE(cpf, apartment_id),
+  UNIQUE(photo_address)
 );
 
 CREATE TABLE ResidentUser (
-    username    VARCHAR (11) PRIMARY KEY,
-    password    VARCHAR (30) NOT NULL,
-    resident_id INTEGER      NOT NULL,
-    FOREIGN KEY (resident_id) REFERENCES Resident(id) ON DELETE CASCADE
+  username    VARCHAR (25) PRIMARY KEY,
+  password    VARCHAR (25) NOT NULL,
+  resident_id INTEGER      REFERENCES Resident(id),
+  UNIQUE(username, password)
 );
 
 CREATE TABLE Employee (
-    id         INTEGER      PRIMARY KEY AUTOINCREMENT,
-    cpf        VARCHAR (11) NOT NULL,
-    name       VARCHAR (40) NOT NULL,
-    age        INTEGER      NOT NULL,
-    role       VARCHAR (20) NOT NULL,
-    complex_id INTEGER      NOT NULL,
-    UNIQUE (cpf, role, complex_id),
-    FOREIGN KEY (complex_id) REFERENCES Complex(id) ON DELETE CASCADE
+  id             INTEGER      PRIMARY KEY,
+  cpf            VARCHAR (11) NOT NULL,
+  name           VARCHAR (50) NOT NULL,
+  birthday       DATE         NOT NULL,
+  photo_address  VARCHAR (200),
+  role           VARCHAR (50) NOT NULL,
+  condominium_id INTEGER      REFERENCES Condominium(id),
+  UNIQUE(cpf, role, condominium_id),
+  UNIQUE(photo_address)
 );
 
 CREATE TABLE EmployeeUser (
-    username    VARCHAR (30) PRIMARY KEY,
-    password    VARCHAR (30) NOT NULL,
-    employee_id INTEGER      NOT NULL,
-    UNIQUE (username, employee_id),
-    FOREIGN KEY (employee_id) REFERENCES Employee(id) ON DELETE CASCADE
+  username    VARCHAR (25) PRIMARY KEY,
+  password    VARCHAR (25) NOT NULL,
+  employee_id INTEGER      REFERENCES Employee(id),
+  UNIQUE(username, password)
 );
 
-CREATE TABLE Shop (
-    id         INTEGER      PRIMARY KEY AUTOINCREMENT,
-    type       VARCHAR (10) NOT NULL,
-    complex_id INTEGER      NOT NULL,
-    UNIQUE (type, complex_id),
-    FOREIGN KEY (complex_id) REFERENCES Complex(id) ON DELETE CASCADE
+CREATE TABLE Rules (
+  id             INTEGER       PRIMARY KEY,
+  text           VARCHAR (500) NOT NULL,
+  condominium_id INTEGER       REFERENCES Condominium(id)
 );
 
-CREATE TABLE Item (
-    id            INTEGER       PRIMARY KEY AUTOINCREMENT,
-    shop_id       INTEGER       NOT NULL,
-    owner         INTEGER       NOT NULL,
-    name          VARCHAR (20)  NOT NULL,
-    price         DECIMAL (9)   NOT NULL,
-    description   VARCHAR (500),
-    images_folder VARCHAR (150),
-    UNIQUE (images_folder),
-    UNIQUE (shop_id, owner, name),
-    FOREIGN KEY (owner)   REFERENCES Apartment(id) ON DELETE CASCADE,
-    FOREIGN KEY (shop_id) REFERENCES Shop(id)      ON DELETE CASCADE
+CREATE TABLE Notification (
+  id             INTEGER       PRIMARY KEY,
+  type           INTEGER       NOT NULL,
+  title          VARCHAR (25)  NOT NULL,
+  text           VARCHAR (300) NOT NULL,
+  finish         DATETIME      NOT NULL,
+  condominium_id INTEGER       REFERENCES Condominium(id)
+);
+
+CREATE TABLE EventType (
+  id             INTEGER      PRIMARY KEY AUTO_INCREMENT,
+  name           VARCHAR (30) NOT NULL,
+  condominium_id INTEGER      REFERENCES Condominium(id),
+  UNIQUE(name, condominium_id)
 );
 
 CREATE TABLE Event (
-    id         INTEGER       PRIMARY KEY AUTOINCREMENT,
-    type       VARCHAR (20)  NOT NULL,
-    title      VARCHAR (30)  NOT NULL,
-    text       VARCHAR (200),
-    complex_id INTEGER       NOT NULL,
-    UNIQUE (type, title, complex_id),
-    FOREIGN KEY (complex_id) REFERENCES Complex(id)
-);
-
-CREATE TABLE ComplexEvent (
-    id          INTEGER  PRIMARY KEY AUTOINCREMENT,
-    from_date   DATETIME NOT NULL,
-    to_date     DATETIME NOT NULL,
-    event_id    INTEGER  NOT NULL,
-    employee_id INTEGER  NOT NULL,
-    UNIQUE (from_date, to_date, event_id),
-    FOREIGN KEY (event_id)    REFERENCES Event(id)    ON DELETE CASCADE,
-    FOREIGN KEY (employee_id) REFERENCES Employee(id) ON DELETE CASCADE
-);
-
-CREATE TABLE ResidentEvent (
-    id          INTEGER  PRIMARY KEY AUTOINCREMENT,
-    from_date   DATETIME NOT NULL,
-    to_date     DATETIME NOT NULL,
-    event_id    INTEGER  NOT NULL,
-    apt_id      INTEGER  NOT NULL,
-    UNIQUE (from_date, to_date, event_id),
-    FOREIGN KEY (event_id) REFERENCES Event(id)     ON DELETE CASCADE,
-    FOREIGN KEY (apt_id)   REFERENCES Apartment(id) ON DELETE CASCADE
+  id            INTEGER  PRIMARY KEY,
+  start         DATETIME NOT NULL,
+  finish        DATETIME NOT NULL,
+  event_type_id INTEGER  REFERENCES EventType(id),
+  apartment_id  INTEGER  REFERENCES Apartment(id),
+  UNIQUE(event_type_id, start, finish)
 );
 
 CREATE TABLE Service (
-    id      INTEGER      PRIMARY KEY AUTOINCREMENT,
-    name    VARCHAR (50) NOT NULL,
-    company VARCHAR (30) NOT NULL,
-    type    VARCHAR (30) NOT NULL,
-    apt_id  INTEGER      NOT NULL,
-    UNIQUE (name, company, type),
-    FOREIGN KEY (apt_id) REFERENCES Apartment(id) ON DELETE CASCADE
+  id            INTEGER      PRIMARY KEY,
+  service_name  VARCHAR (30) NOT NULL,
+  employee_name VARCHAR (50),
+  arrival       DATETIME,
+  apartment_id  INTEGER      REFERENCES Apartment(id)
 );
 
-CREATE TABLE ServiceDay (
-    id           INTEGER  PRIMARY KEY AUTOINCREMENT,
-    service_id   INTEGER  NOT NULL,
-    weekday      INTEGER  NOT NULL,
-    from_date    DATETIME NOT NULL,
-    to_date      DATETIME NOT NULL,
-    UNIQUE (service_id, weekday, from_date, to_date),
-    FOREIGN KEY (service_id) REFERENCES Service(id)
+CREATE TABLE Guest (
+  id           INTEGER      PRIMARY KEY,
+  guest_name   VARCHAR (50) NOT NULL,
+  arrival      DATETIME,
+  apartment_id INTEGER      REFERENCES Apartment(id)
 );
-
-CREATE TABLE Rule (
-    id         INTEGER       PRIMARY KEY AUTOINCREMENT,
-    text       VARCHAR (150) NOT NULL,
-    complex_id INTEGER       NOT NULL,
-    UNIQUE (text, complex_id),
-    FOREIGN KEY (complex_id) REFERENCES Complex(id) ON DELETE CASCADE
-);
-
-CREATE TABLE Warning (
-    id         INTEGER       PRIMARY KEY AUTOINCREMENT,
-    type       INTEGER       NOT NULL,
-    text       VARCHAR (150) NOT NULL,
-    tower_id   INTEGER       NOT NULL,
-    UNIQUE (text, tower_id),
-    FOREIGN KEY (tower_id) REFERENCES Tower(id) ON DELETE CASCADE
-);
-
-
-ALTER TABLE Complex   ADD INDEX Complex  (name)                USING HASH;
-ALTER TABLE Tower     ADD INDEX Tower    (name)                USING HASH;
-ALTER TABLE Apartment ADD INDEX Apartment(number)              USING HASH;
-ALTER TABLE Event     ADD INDEX Event    (type, title)         USING BTREE;
-ALTER TABLE Service   ADD INDEX Service  (name, company, type) USING BTREE;
-ALTER TABLE Shop      ADD INDEX Shop     (type)                USING BTREE;
