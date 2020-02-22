8bb3732e7bcb8483f0ab39dfc80fd8211e6ccda9
jojo0severo <joaovbsevero@gmail.com>
2020-02-22 01:26:27 -0300

    Added address to the insertion

--- helpers/condominium_builder.py
+++ helpers/condominium_builder.py
@@ -1,8 +1,12 @@
+from model.country import Country
+from model.state import State
+from model.city import City
+from model.address import Address
 from model.condominium import Condominium
 from model.tower import Tower
 from model.apartment import Apartment
 from setup import db
-from sqlalchemy import exc
+from sqlalchemy import exc, and_
 
 
 def parse_tower_list(tower_list_obj, condominium_id):
@@ -38,13 +42,51 @@ def parse_apartments(tower_obj, tower_id):
                 db.session.add(Apartment(apt_number=apt_number + j, tower_id=tower_id))
 
 
+def build_address(condominium_obj):
+    try:
+        country = Country(name=condominium_obj['country_name'])
+        db.session.add(country)
+
+    except exc.IntegrityError:
+        db.session.rollback()
+        country = Country.query.filter_by(name=condominium_obj['country_name'])
+
+    try:
+        state = State(name=condominium_obj['state_name'], country_id=country.id)
+        db.session.add(state)
+
+    except exc.IntegrityError:
+        db.session.rollback()
+        state = State.query.filter(and_(State.name == condominium_obj['state_name'], State.country_id == country.id))
+
+    try:
+        city = City(name=condominium_obj['city_name'], state_id=state.id)
+        db.session.add(city)
+
+    except exc.IntegrityError:
+        db.session.rollback()
+        city = City.query.filter(and_(City.name == condominium_obj['city_name'], City.state_id == state.id))
+
+    try:
+        address = Address(street_name=condominium_obj['street_name'], neighbourhood=condominium_obj['neighbourhood'], city_id=city.id)
+        db.session.add(address)
+
+        return address.id
+
+    except exc.IntegrityError as e:
+        db.session.rollback()
+        raise ValueError(e)
+
+
 def build(json_structure):
     for condominium in json_structure:
+        condominium_obj = json_structure[condominium]
 
-        condominium = Condominium(name=condominium, address_id=1)
+        address_id = build_address(condominium_obj)
+
+        condominium = Condominium(name=condominium, address_id=address_id)
         db.session.add(condominium)
 
-        condominium_obj = json_structure[condominium]
         for key in condominium_obj:
             if key == 'Towers':
                 parse_tower_list(condominium_obj[key], condominium.id)
